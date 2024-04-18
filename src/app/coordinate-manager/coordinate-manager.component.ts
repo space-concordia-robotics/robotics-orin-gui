81a0790726c73e79da09d906961a53e434f9911e
steamypotato <hoopahydra@gmail.com>
2024-04-18 16:51:53 -0400

    Added some stuff

--- src/app/coordinate-manager/coordinate-manager.component.ts
+++ src/app/coordinate-manager/coordinate-manager.component.ts
@@ -9,23 +9,16 @@ import {
   MatRow, MatRowDef,
   MatTable
 } from "@angular/material/table";
-export interface PeriodicElement {
+import {MatInput} from "@angular/material/input";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+export interface CoordinateElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  latitude: number;
+  longitude: number;
+  status: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: CoordinateElement[] = [
+  {name: "GPS1", latitude: 1.22123, longitude: 1.0079, status: 'H'},
 ];
 
 @Component({
@@ -43,13 +36,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatHeaderCellDef,
     MatCellDef,
     MatHeaderRowDef,
-    MatRowDef
+    MatRowDef,
+    MatInput,
+    ReactiveFormsModule
   ],
   templateUrl: './coordinate-manager.component.html',
   styleUrl: './coordinate-manager.component.css'
 })
 
 export class CoordinateManagerComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'latitude', 'longitude', 'status'];
+  coordinatesForm : FormControl
   dataSource = ELEMENT_DATA;
 }
