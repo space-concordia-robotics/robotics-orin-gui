8e874c2df1aaf6805698869965fcbd9b32292f9e
AmirDehestani <amira.dehestani@gmail.com>
2024-06-11 17:02:43 -0400

    Fix the image rerendering bug

--- src/app/ar-ui/ar-display/ar-display.component.ts
+++ src/app/ar-ui/ar-display/ar-display.component.ts
@@ -14,51 +14,81 @@ import { NgForOf } from '@angular/common';
   styleUrl: './ar-display.component.css'
 })
 export class ArDisplayComponent {
-  // tags : ArucoTag[];
+  tags : ArucoTag[] = [];
   
   // For testing
-  tags : ArucoTag[] = [{
-    id : 1,
-    pose : new ROSLIB.Pose ({
-      position: new ROSLIB.Vector3({
-        x: 1.0,
-        y: 2.0,
-        z: 0.5
-      }),
-      orientation: new ROSLIB.Quaternion({
-        x: 10.0,
-        y: 0.5,
-        z: 0.0,
-        w: 1.0
-      })
-    })
-  },{
-    id : 2,
-    pose : new ROSLIB.Pose ({
-      position: new ROSLIB.Vector3({
-        x: 6.0,
-        y: 2.5,
-        z: 7.5
-      }),
-      orientation: new ROSLIB.Quaternion({
-        x: 4.0,
-        y: 8.0,
-        z: 11.25,
-        w: 1.5
-      })
-    })
-  }]
+  // tags : ArucoTag[] = [{
+  //   id : 1,
+  //   pose : new ROSLIB.Pose ({
+  //     position: new ROSLIB.Vector3({
+  //       x: 1.0,
+  //       y: 2.0,
+  //       z: 0.5
+  //     }),
+  //     orientation: new ROSLIB.Quaternion({
+  //       x: 10.0,
+  //       y: 0.5,
+  //       z: 0.0,
+  //       w: 1.0
+  //     })
+  //   })
+  // },{
+  //   id : 2,
+  //   pose : new ROSLIB.Pose ({
+  //     position: new ROSLIB.Vector3({
+  //       x: 6.0,
+  //       y: 2.5,
+  //       z: 7.5
+  //     }),
+  //     orientation: new ROSLIB.Quaternion({
+  //       x: 4.0,
+  //       y: 8.0,
+  //       z: 11.25,
+  //       w: 1.5
+  //     })
+  //   })
+  // }]
 
   constructor(private rosService: RosService) {}
 
+  // ngOnInit(): void {
+  //   this.rosService.subscribeToArucoMarkers((data: ArucoMarker) => {
+  //     this.tags = data.marker_ids.map((id, i) => {
+  //       return {
+  //         id: id,
+  //         pose: data.poses[i]
+  //       }
+  //     })
+  //   });
+  // }
+
   ngOnInit(): void {
     this.rosService.subscribeToArucoMarkers((data: ArucoMarker) => {
-      this.tags = data.marker_ids.map((id, i) => {
-        return {
-          id: id,
-          pose: data.poses[i]
+      const newTagIds = data.marker_ids;
+      const newPoses = data.poses;
+
+      const newTagsMap = new Map<number, ROSLIB.Pose>();
+      newTagIds.forEach((id, index) => {
+        newTagsMap.set(id, newPoses[index]);
+      });
+
+      // Update tags
+      this.tags = this.tags.filter(tag => {
+        if (newTagsMap.has(tag.id)) {
+          // If it still exists, update the pose
+          tag.pose = newTagsMap.get(tag.id)!;
+          newTagsMap.delete(tag.id);
+          return true;
+        } else {
+          // If it doesn't exist anymore, remove it
+          return false;
         }
-      })
+      });
+
+      // Add new tags
+      newTagsMap.forEach((pose, id) => {
+        this.tags.push({ id, pose });
+      });
     });
   }
 }
