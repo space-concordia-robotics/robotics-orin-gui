0aa5577631b5f2bd393a0a84f2ac7957a2da816e
AmirDehestani <amira.dehestani@gmail.com>
2024-06-13 18:02:40 -0400

    Make AR markers disappear if they're no longer in view

--- src/app/ar-ui/ar-display/ar-display.component.ts
+++ src/app/ar-ui/ar-display/ar-display.component.ts
@@ -15,52 +15,8 @@ import { NgForOf } from '@angular/common';
 })
 export class ArDisplayComponent {
   tags : ArucoTag[] = [];
-  
-  // For testing
-  // tags : ArucoTag[] = [{
-  //   id : 1,
-  //   pose : new ROSLIB.Pose ({
-  //     position: new ROSLIB.Vector3({
-  //       x: 1.0,
-  //       y: 2.0,
-  //       z: 0.5
-  //     }),
-  //     orientation: new ROSLIB.Quaternion({
-  //       x: 10.0,
-  //       y: 0.5,
-  //       z: 0.0,
-  //       w: 1.0
-  //     })
-  //   })
-  // },{
-  //   id : 2,
-  //   pose : new ROSLIB.Pose ({
-  //     position: new ROSLIB.Vector3({
-  //       x: 6.0,
-  //       y: 2.5,
-  //       z: 7.5
-  //     }),
-  //     orientation: new ROSLIB.Quaternion({
-  //       x: 4.0,
-  //       y: 8.0,
-  //       z: 11.25,
-  //       w: 1.5
-  //     })
-  //   })
-  // }]
 
   constructor(private rosService: RosService) {}
-
-  // ngOnInit(): void {
-  //   this.rosService.subscribeToArucoMarkers((data: ArucoMarker) => {
-  //     this.tags = data.marker_ids.map((id, i) => {
-  //       return {
-  //         id: id,
-  //         pose: data.poses[i]
-  //       }
-  //     })
-  //   });
-  // }
 
   ngOnInit(): void {
     this.rosService.subscribeToArucoMarkers((data: ArucoMarker) => {
--- src/app/ar-ui/ar-tag/ar-tag.component.ts
+++ src/app/ar-ui/ar-tag/ar-tag.component.ts
@@ -1,21 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { ArucoTag } from '../../interfaces/aruco-tag';
-import { NgForOf, NgIf } from '@angular/common';
+import { NgClass, NgForOf, NgIf } from '@angular/common';
 import { ArTagService } from '../../services/ar-tag.service';
+import { Pose } from 'roslib';
 
 @Component({
   selector: 'app-ar-tag',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    NgClass
   ],
   templateUrl: './ar-tag.component.html',
   styleUrl: './ar-tag.component.css',
 })
 export class ArTagComponent {
-  @Input() tag : ArucoTag
-  image_path : string
+  @Input() tag : ArucoTag;
+  image_path : string;
+  isVisible: boolean = true;
+  timer: ReturnType<typeof setTimeout>;
+  previousPose: Pose;
 
   constructor(private arTagService : ArTagService) {}
 
@@ -23,5 +28,29 @@ export class ArTagComponent {
     this.arTagService.generateArucoTag(this.tag.id).subscribe(res => {
       this.image_path = res.imagePath;
     })
+
+    // Keep track of the prevoius pose to detect if the pose has not been updated for some time
+    this.previousPose = this.tag.pose
+
+    this.resetTimer();
+  }
+
+  ngDoCheck() {
+    if (this.tag.pose !== this.previousPose) {
+      this.previousPose = this.tag.pose
+      this.resetTimer();
+    }
+  }
+
+  resetTimer(): void {
+    this.isVisible = true;
+
+    // Clear timeout based on the id, if exists
+    clearTimeout(this.timer);
+
+    // Save the timeout id
+    this.timer = setTimeout(() => {
+      this.isVisible = false;
+    }, 2000)
   }
 }
